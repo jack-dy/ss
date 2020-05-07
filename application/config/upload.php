4571fe4dc6d36a483f5f8e90c415ddfd6c7c494a
dy <631271745@qq.com>
2020-05-07 10:59:51 +0800

    init

--- application/config/upload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config['upload_path'] = './static/uploads/';
-$config['allowed_types'] = 'gif|jpg|png';
-$config['max_size'] = '2048';
-$config['file_name'] = date('YmdHis').rand(10,99);
-?>
